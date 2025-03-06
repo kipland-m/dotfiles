4e94acb3684d798bf6d76c37187f4e9ff5a4b90e
Kipland Melton <kiplandmelton@gmail.com>
2025-03-06 00:04:05 -0600

    update nvim

--- init.lua
+++ init.lua
@@ -1,11 +1,21 @@
 require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
+    -- test comment after github outage
 
     -- manage mason
     use {
       "williamboman/mason.nvim",
       run = ":MasonUpdate" -- update registry stuff
+    }
+
+    use {
+      'andweeb/presence.nvim'
+    }
+
+    -- manage nvim-treesitter
+    use {
+      "nvim-treesitter/nvim-treesitter"
     }
 
     -- manage alpha-nvim (splash screen)
@@ -42,7 +52,6 @@ require('packer').startup(function(use)
     use 'nvim-tree/nvim-tree.lua'
     vim.keymap.set('n', '©', ':NvimTreeOpen<CR>', { noremap = true, silent = true })
 
-    vim.cmd("colorscheme desert")    
     vim.opt.number = true          -- Show line numbers
     vim.opt.tabstop = 2            -- Number of spaces a tab character occupies
     vim.opt.shiftwidth = 2         -- Number of spaces for each level of indentation
@@ -127,9 +136,51 @@ cmp.setup({
     capabilities = capabilities
   }
 
-require("nvim-tree").setup()
+require("nvim-tree").setup {
+  filters = {
+    dotfiles = false,
+}}
 
 require("nvim-web-devicons").setup {
   default = true;
 }
 
+require('packer').startup(function()
+  use 'navarasu/onedark.nvim'
+end)
+
+require('onedark').setup {
+  style = 'dark', -- Options: dark, darker, cool, deep, warm, warmer
+  transparent = true,
+}
+
+require('onedark').load()
+
+require("presence").setup({
+    -- General options
+    auto_update         = true,                       -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
+    neovim_image_text   = "The One True Text Editor", -- Text displayed when hovered over the Neovim image
+    main_image          = "neovim",                   -- Main image display (either "neovim" or "file")
+    client_id           = "793271441293967371",       -- Use your own Discord application client id (not recommended)
+    log_level           = nil,                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
+    debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+    enable_line_number  = false,                      -- Displays the current line number instead of the current project
+    blacklist           = {},                         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
+    buttons             = true,                       -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
+    file_assets         = {},                         -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
+    show_time           = true,                       -- Show the timer
+
+    -- Rich Presence text options
+    editing_text        = "Editing %s",               -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+    file_explorer_text  = "Browsing %s",              -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+    git_commit_text     = "Committing changes",       -- Format string rendered when committing changes in git (either string or function(filename: string): string)
+    plugin_manager_text = "Managing plugins",         -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+    reading_text        = "Reading %s",               -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+    workspace_text      = "Working on %s",            -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+    line_number_text    = "Line %s out of %s",        -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+})
+
+require('nvim-treesitter.configs').setup {
+  ensure_installed = {"c"}, -- Add more languages if needed
+  highlight = { enable = true },
+}
